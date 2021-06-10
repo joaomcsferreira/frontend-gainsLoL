1913daaa296f68b8c72d6d5d0a2fdc7f1fdd1bd0
Joao-MCF <marcosc024@gmail.com>
2021-06-10 15:44:12 -0300

    Added global style and color palette

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
 <script>
 
+import './style/global.css'
+
 import Header from './components/Header'
 
 export default {
@@ -16,7 +18,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
