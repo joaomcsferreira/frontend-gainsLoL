7687d59c488a97854d0838ea4a1d66c7aea6c04f
João Marcos <56234707+Joao-MCF@users.noreply.github.com>
2021-06-10 14:35:10 -0300

    Add/header component (#7)
    * added font awesome
    
    * Added import from Header
    
    * Added Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+
+        <nav class="navbar navbar-expand navbar-light mb-20">
+            <div class="container-fluid">
+                <a class="display-4 text-dark text-decoration-none" href="/">GainsLoL</a>
+
+                <div id="menu-login">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">teste@teste.com</a>
+                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/"><i class="fas fa-user-circle"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/"><i class="fas fa-cog"></i></a>
+                    </li>
+                </ul>
+            </div>
+                
+            </div>
+        </nav>
+
+        <span id=line></span>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style scoped>
+    #line {
+        display: block;
+        border-bottom: 1.5px solid #cecece;
+        margin: 0 50px;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 Vue.config.productionTip = false
 
