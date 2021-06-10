87b8fff966547dc8fed08658c1fd169963a81984
Joao-MCF <marcosc024@gmail.com>
2021-06-10 13:50:57 -0300

    Added import from Header

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <div id="app">
+    <Header />
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header'
+
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
